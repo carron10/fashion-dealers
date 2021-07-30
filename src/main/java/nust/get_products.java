4ba734d404be53fcb07ac3e9a90385644291c2ae
Carron Muleya <carronmuleya10@gmail.com>
2021-07-30 15:10:57 +0200

    kjhg

--- src/main/java/nust/get_products.java
+++ src/main/java/nust/get_products.java
@@ -18,6 +18,7 @@ import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,6 +27,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Muleya
  */
+@WebServlet(name = "get_product", urlPatterns = {"/get_products"})
 public class get_products extends HttpServlet {
 
     /**
