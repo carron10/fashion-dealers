aa46c10b0497bf726380d0342954ca09b880c8fb
Carron Muleya <carronmuleya10@gmail.com>
2021-09-09 04:45:48 +0200

    Removing the page loader and solving cookie isssues

--- src/main/webapp/scripts/cart.js
+++ src/main/webapp/scripts/cart.js
@@ -23,6 +23,6 @@ function addToCart(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/cart.jsp";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
    updateCart();
 }
